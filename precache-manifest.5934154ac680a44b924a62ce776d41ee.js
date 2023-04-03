d42020e246a8d1fce75c255c869714f6cf9e339b
ammusandra13@gmail.com <MY_NAME@example.com>
2023-04-04 01:58:51 +0530

    Updates

---
+++ precache-manifest.5934154ac680a44b924a62ce776d41ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eef8208651fc2610b433644657c42f2c",
+    "url": "/game-of-motivation/index.html"
+  },
+  {
+    "revision": "5027885c01381ca127f5",
+    "url": "/game-of-motivation/static/css/main.f14595a4.chunk.css"
+  },
+  {
+    "revision": "5a0b763e6a60d36dcc7e",
+    "url": "/game-of-motivation/static/js/2.ba15851d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-of-motivation/static/js/2.ba15851d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5027885c01381ca127f5",
+    "url": "/game-of-motivation/static/js/main.3b2db887.chunk.js"
+  },
+  {
+    "revision": "c96e173a10b378921c16",
+    "url": "/game-of-motivation/static/js/runtime-main.f6d1806a.js"
+  },
+  {
+    "revision": "156ffd60ec82cdaa94d6655563f8bdfd",
+    "url": "/game-of-motivation/static/media/paper.156ffd60.jpg"
+  }
+]);
